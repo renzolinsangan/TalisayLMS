1f057e197ca32fabb1064c6a5a4616b7bdebef5b
renzolinsangan <renzolinsangan11@gmail.com>
2023-10-06 20:14:41 +0800

    10-6-2023-admin login has been changed.

--- admin/admin_info.php
+++ admin/admin_info.php
@@ -24,35 +24,35 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         exit();
     } else {
         // Perform login validation using the submitted username and password
-        $sql = "SELECT * FROM users WHERE user_name = ? AND password = ?";
+        $sql = "SELECT * FROM users WHERE user_name = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ss", $name, $pass);
+        $stmt->bind_param("s", $name);
         $stmt->execute();
         $result = $stmt->get_result();
         $row = $result->fetch_assoc();
 
         if ($row) {
-            // Close the connection
-            $stmt->close();
-            $conn->close();
-
-            $_SESSION['id'] = $row['id'];
-            $_SESSION['name'] = $row['user_name'];
-
-            if ($_SESSION['name'] === "principal" || $_SESSION['name'] === "guidance") {
+            // Check if the entered password matches the stored hashed password
+            if (password_verify($pass, $row['password'])) {
+                // Password is correct, store user ID in session
+                $_SESSION['id'] = $row['admin_id'];
+                $_SESSION['name'] = $row['user_name'];
                 header("Location: index.php");
             } else {
+                // Password is incorrect, show error message
                 $_SESSION['entered_username'] = $name; // Store the entered username
-                header("Location: admin_login.php?error=Incorrect Username or Password!");
+                header("Location: admin_login.php?error=Incorrect Password!");
             }
-
             exit();
         } else {
+            // Username not found in the database
+            $_SESSION['entered_username'] = $name; // Store the entered username
             header("Location: admin_login.php?error=Incorrect Username or Password!");
             exit();
         }
     }
 } else {
+    // Redirect to the login page if no POST data is received
     header("Location: admin_login.php");
     exit();
 }
